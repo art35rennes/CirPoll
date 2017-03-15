47f5df9ce00b2dd531d5a29a364be48e4115ca86
Arthur Sicard <arthur2rennes@hotmail.fr>
2017-03-15 14:18:40 +0100

    2

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,4 @@
 // JavaScript Document
 'use strict';
 
-ajaxRequest("GET", "php/request.php/module/polls/list", console.log('Hello World !'));
+ajaxRequest("GET", "test", maFonctionCallback);
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,51 @@
 // JavaScript Document
 'use strict';
 
-function ajaxRequest(type, request, callback){}
+function ajaxRequest(type, request, callback){
+	var xhr;
+	
+	xhr = new XMLHttpRequest();
+	xhr.open(type, request, true);
+	
+	var divErrors = document.getElementById('errors')
+	var text = "<div id='erroes' class='alert alert-danger' role='alert'>";
+	text += "<span class='glyphicon glypicon-exclamation-sign'></span>";
+	
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState !== 4){
+			return;
+		}
+		switch(xhr.status){
+			case 200:
+				text += "<strong>OK</strong>";
+				break;
+			case 400:
+				text += "<strong>Requete incorrecte</strong>";
+				break;
+			case 401:
+				text += "<strong>Unauthorized</strong>";
+				break;
+			case 403:
+				text += "<strong>Forbidden </strong>";
+				break;
+			case 500:
+				text += "<strong>Internal Error</strong>";
+				break;
+			case 404:
+				text += "<strong>Not found</strong>";
+				break;
+		}
+		text +="</div";
+		divErrors.innerHTML = text;
+	};
+	
+	xhr.send();
+	
+}
+
+function httpErrors(errorNumber){
+	console.log(errorNumber);
+}
 
 estSuperieur(25, 2, maFonctionCallback); // Affiche 'oui'.
 
